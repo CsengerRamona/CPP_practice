372b7d9090665fe0bbf249d5b4e5749bdc1875c8
AD001\Z004M4BS <ramona.csenger.ext@siemens.ro>
2022-08-07 21:20:01 +0300

    Modified 08_lib_ini_read_book

--- 08_lib_ini_read_book/src/main_ini_bookread.cpp
+++ 08_lib_ini_read_book/src/main_ini_bookread.cpp
@@ -43,10 +43,45 @@ public:
 	@param file_name The name of the file to read from (must include path).
 	@return Vector of books.
 */
-std::vector<Book> readBooksFromIniFile(const std::string& file_name)
+std::vector<Book> readBooksFromIniFile(char* file_name)
 {
 	std::vector<Book> results;
+	Book myBook;
 	// TODO: BEGIN read the file -------------------------------------
+
+	CSimpleIniA ini;
+	ini.SetUnicode();
+
+	SI_Error rc = ini.LoadFile(file_name);
+
+	//CSimpleIniA::TNamesDepend sections;
+	//ini.GetAllSections(sections);
+
+	//CSimpleIniA::TNamesDepend keys;
+	// ini.GetAllKeys("books", keys);
+	const int booksCount = (*ini.GetValue("books", "count")) - 48;
+	if (rc < 0) {
+		std::cout << "Error when reading the file" << std::endl;/* handle error */
+	};
+
+
+
+	for (int i = 0; i < booksCount; i++)
+	{	
+		std::stringstream ss;
+		ss << "book." << (i + 1);
+		std::string section_name(ss.str());
+		const char* charArray = section_name.c_str();
+		myBook.name = ini.GetValue(charArray, "name");
+		myBook.authors = ini.GetValue(charArray,"author");
+
+
+		results.push_back(myBook);
+	}
+	//const char* pv;
+	//pv = ini.GetValue("book.1", "author");
+	//std::cout << pv << std::endl;
+
 	
 	// E.g. Book myBook;
 	//		// build the section name (E.g. book.1)
@@ -68,9 +103,11 @@ int main()
 	// Using the SimpleINI C++ Lib: https://github.com/brofield/simpleini
 
 	// Read the data
-	std::string input_data("PATH_TO_INI_FILE.ini");
+	//std::string input_data("D:/Cursuri/CursCpp/Tema1/CPP_practice/data/ermahgerd_berks.ini");
+	char* input_data("D:/Cursuri/CursCpp/Tema1/CPP_practice/data/ermahgerd_berks.ini");
 	std::cout << "Reading the data from " << input_data << std::endl;
 	std::vector<Book> books_from_file = readBooksFromIniFile(input_data);
+
 
 	// Print the data
 	std::cout << "Here are all the books found in the data file..." << std::endl;
